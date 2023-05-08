428fa5074a8860d27a4d340f3dff9a7e1d90effc
Giuliana <giu.diast@gmail.com>
2023-05-08 14:43:11 -0300

    solução exercicio 2

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicio-02.js
@@ -0,0 +1,21 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+     {
+        titulo: 'O pior dia de todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+     }
+    ]
+};
+//console.log('O livro favorito de', leitor['nome'], leitor['sobrenome'], 'se chama "', leitor['livrosFavoritos'][0]['titulo'], '"');
+leitor.livrosFavoritos.push(
+    { 
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+    }
+)
+//console.log(leitor.nome, 'tem', leitor.livrosFavoritos.length, 'livros favoritos.');
